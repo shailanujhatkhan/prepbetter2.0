d63598c8a2bca50015a1044ebde6be2f3bd46715
Shaila Nujhat Khan <shaila.nujhat.khan@g.bracu.ac.bd>
2026-05-03 02:51:31 +0600

    trying to fix seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => \fake()->name(),
-            'email' => \fake()->unique()->safeEmail(),
+            'name' => 'Demo User ' . Str::random(6),
+            'email' => 'user_' . Str::random(12) . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
